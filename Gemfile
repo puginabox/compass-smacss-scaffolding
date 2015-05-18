fe26cd0ecf944c6ce5c02098332e3dfc85a4f637
Griffin Byron <griffin.byron@strayer.edu>
2015-05-18 13:13:43 -0400

    tidying up partials

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
-# A sample Gemfile
 source "https://rubygems.org"
-
-# gem "rails"
+gem 'compass', '~> 1.0.0.alpha.20'
+gem "sass", '~> 3.4.0.rc.1'
+gem "singularitygs", '>= 1.3'
+gem "breakpoint"
--- config.rb
+++ config.rb
@@ -1,10 +1,10 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "scss"
+sass_dir = "Sass"
 images_dir = "images"
 javascripts_dir = "js"
 
 sass_options = {:sourcemap => false}
-
+require 'singularitygs'
 
